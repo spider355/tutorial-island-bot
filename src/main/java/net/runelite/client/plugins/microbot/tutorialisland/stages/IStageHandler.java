630a786e16add887126460d2ab0826dbf8bb7008
spider355 <43102031+spider355@users.noreply.github.com>
2026-02-07 14:09:49 +1300

    Add IStageHandler interface for Tutorial Island plugin

---
+++ src/main/java/net/runelite/client/plugins/microbot/tutorialisland/stages/IStageHandler.java
@@ -0,0 +1,7 @@
+package net.runelite.client.plugins.microbot.tutorialisland.stages;
+
+import net.runelite.client.plugins.microbot.tutorialisland.TutorialIslandConfig;
+
+public interface IStageHandler {
+    boolean execute(TutorialIslandConfig config);
+}
